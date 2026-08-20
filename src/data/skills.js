e04a1bf00f0e18f4710f33d39fbd1656aaa59b6f
Juan Pablo Millicay <juan22milli@gmail.com>
2026-08-20 01:23:38 -0300

    Datos de Skills

---
+++ src/data/skills.js
@@ -0,0 +1,8 @@
+export const skills = [
+  { id: 1, name: "HTML5 & CSS3", level: "Avanzado", isFavorite: false },
+  { id: 2, name: "JavaScript (ES6+)", level: "Intermedio", isFavorite: true },
+  { id: 3, name: "React", level: "Aprendiendo", isFavorite: true },
+  { id: 4, name: "Tailwind CSS", level: "Intermedio", isFavorite: false },
+  { id: 5, name: "Git & GitHub", level: "Intermedio", isFavorite: false },
+  { id: 6, name: "Visual Studio Code", level: "Intermedio", isFavorite: true }
+];
